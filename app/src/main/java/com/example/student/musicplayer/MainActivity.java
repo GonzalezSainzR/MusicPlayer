4a9769ead9dd2f1ba8849967d86a557cb79b882c
Renan Gonzalez-Sainz <raygs2000@gmail.com>
2016-11-18 15:01:48 -0800

    Yes

--- app/src/main/java/com/example/student/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/student/musicplayer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.student.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private MediaPlayer song1;
+
+    public void onCreate(){
+        song1 = MediaPlayer.create(this, R.raw.BargainsInATuxedo);
+    }
+
+    public void playSong(View view) {
+        song1.start();
+    }
+    public void pauseSong(View view) {
+        song1.pause();
     }
 }
